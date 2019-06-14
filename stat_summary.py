fd7d90f569c5ca5950ade46b5d8be636fedefe35
Noah Legall <40697188+noahaus@users.noreply.github.com>
2019-06-14 10:33:54 -0400

    use of subprocess library python

--- stat_summary.py
+++ stat_summary.py
@@ -7,6 +7,7 @@
 
 import sys # use to access arguments
 import os # use in order to call commands from terminal script is called in
+import subprocess as sp
 
 #this program will create the stats files that data will be extracted from.
 #if there are 15 samples studied then there will be 15 rows in the outputted csv.
@@ -52,12 +53,12 @@ for i in range(len(R1_list)):
     os.system(generate_fastq_stats.format(R1_list[i],output_R1))
     os.system(generate_fastq_stats.format(R2_list[i],output_R2))
     print("{} and {} created.".format(output_R1,output_R2))
-    R1_size = os.popen("wc -c {}".format(output_R1))
-    R2_size = os.popen("wc -c {}".format(output_R2))
-    Q_ave_R1 = os.popen("cat {} | sed \"1d\" | awk '{{sum+=$6}} END{{print sum/NR}}'".format(output_R1))
-    Q_ave_R2 = os.popen("cat {} | sed \"1d\" | awk '{{sum+=$6}} END{{print sum/NR}}'".format(output_R2))
-    R1_ave_read_length = os.popen("awk '{{if(NR%4==2) {{count++; bases += length}} }} END{{print bases/count}}' {}".format(R1_list[i]))
-    R2_ave_read_length = os.popen("awk '{{if(NR%4==2) {{count++; bases += length}} }} END{{print bases/count}}' {}".format(R2_list[i]))    
+    R1_size = sp.check_output("wc -c {}".format(output_R1),shell=True)
+    R2_size = sp.check_output("wc -c {}".format(output_R2))
+    Q_ave_R1 = sp.check_output("cat {} | sed \"1d\" | awk '{{sum+=$6}} END{{print sum/NR}}'".format(output_R1),shell=True)
+    Q_ave_R2 = sp.check_output("cat {} | sed \"1d\" | awk '{{sum+=$6}} END{{print sum/NR}}'".format(output_R2),shell=True
+    R1_ave_read_length = sp.check_output("awk '{{if(NR%4==2) {{count++; bases += length}} }} END{{print bases/count}}' {}".format(R1_list[i]),shell=True)
+    R2_ave_read_length = sp.check_output("awk '{{if(NR%4==2) {{count++; bases += length}} }} END{{print bases/count}}' {}".format(R2_list[i]),shell=True)    
     os.system("echo \"{},{},{},{},{},{},{}\" >> read_stats.csv".format(sample_name,R1_size,R2_size,Q_ave_R1,Q_ave_R2,R1_ave_read_length,R2_ave_read_length))
 
 os.system("mv read_stats.csv -t {}".format(stats_dir))
@@ -74,11 +75,11 @@ for line in bam:
 os.system('touch bam_stats.csv')
 os.system('echo "total_mapped_reads,ave_coverage,unmapped_reads" >> bam_stats.csv')
 for i in range(len(bam_list)):
-    ave_coverage = os.popen("samtools depth {} | awk '{{sum+=$3}} END {{ print sum/NR}}'".format(bam_list[i]))
-    total_reads = os.popen("samtools flagstat {} | awk -F " " 'NR==1 {{print $1}}'".format(bam_list[i]))
-    mapped_reads = os.popen("samtools flagstat {} | awk -F " " 'NR==1 {{print $5}}'".format(bam_list[i]))
+    ave_coverage = sp.check_output("samtools depth {} | awk '{{sum+=$3}} END {{ print sum/NR}}'".format(bam_list[i]),shell=True)
+    total_reads = sp.check_output("samtools flagstat {} | awk -F " " 'NR==1 {{print $1}}'".format(bam_list[i]),shell=True)
+    mapped_reads = sp.check_output("samtools flagstat {} | awk -F " " 'NR==1 {{print $5}}'".format(bam_list[i]),shell=True)
     unmapped_reads = total_reads - mapped_reads
     os.system("echo \"{},{},{}\" >> bam_stat.csv".format(mapped_reads,ave_coverage,unmapped_reads))
 os.system("mv bam_stats.csv -t {}".format(stats_dir))
 os.chdir(stats_dir)
-os.system("paste -d \",\" read_stats.csv bam_stats.csv")
+os.system("paste -d \",\" read_stats.csv bam_stats.csv > stat_summary.csv")
