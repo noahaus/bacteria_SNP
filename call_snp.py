b3e5c7dcd64248f7c736edcf61b5f5060120c61f
Noah Legall <noahaus@uga.edu>
2019-07-10 16:03:14 -0400

    make verbose

--- call_snp.py
+++ call_snp.py
@@ -21,15 +21,16 @@ for line in bam:
     bam_list.append(line.strip())
 
 print("indexing the bam files (if necessary)")
+os.system("rm samp_list.txt")
 for i in range(len(bam_list)):
     output = bam_list[i].replace(".nodup.sorted.bam",".addsample.bam")
     sm = bam_list[i].replace(".nodup.sorted.bam","")
     print("INDEXING: {}".format(bam_list[i]))
     os.system("samtools index {}".format(bam_list[i]))
+    print("ADDING READ GROUP: {}".format(bam_list[i]))
     os.system("java -jar $EBROOTPICARD/picard.jar AddOrReplaceReadGroups INPUT={} OUTPUT={} RGLB=lib1 RGPL=illumina RGPU=unit1  RGSM={} USE_JDK_DEFLATER=true USE_JDK_INFLATER=true".format(bam_list[i],output,sm))
     os.system("echo {} >> samp_list.txt".format(output))
     os.system("samtools index {}".format(output))
-
 
 samp_list = []
 samp = open('samp_list.txt','r')
@@ -40,20 +41,24 @@ for line in samp:
 #base calling with freebayes-parallel. hard filtering, keep only SNPs that passed.
 intersect_isolates = ""
 for i in range(len(samp_list)):
+    print("\n\n\n")
     raw_output = samp_list[i].replace(".addsample.bam",".unfiltered.vcf")
+    print("SNP CALLING: freebayes-parallel chrom_ranges.txt 8 -E -1 -e 1 -u --strict-vcf -f {}  {} > {}".format(ref_genome.strip(),samp_list[i],raw_output))
     os.system("freebayes-parallel chrom_ranges.txt 8 -E -1 -e 1 -u --strict-vcf -f {}  {} > {}".format(ref_genome.strip(),samp_list[i],raw_output))
     filtered_output = samp_list[i].replace(".addsample.bam",".filtered.vcf")
+    print("HARD FILTERING: bcftools filter -s LowQual -i '%QUAL>150 && TYPE=\"snp\" && INFO/DP > 10' {} > {}".format(raw_output,filtered_output))
     os.system("bcftools filter -s LowQual -i '%QUAL>150 && TYPE=\"snp\" && INFO/DP > 10' {} > {}".format(raw_output,filtered_output))
     pass_output = samp_list[i].replace(".addsample.bam",".only_pass.vcf")
     os.system("grep -v \"LowQual\" {} > {}".format(filtered_output,pass_output))
     os.system("bgzip {}".format(pass_output))
     os.system("bcftools index {}.gz".format(pass_output))
     intersect_isolates = intersect_isolates+" {}.gz".format(pass_output)
-
+    print("\n\n\n")
 #intersect all the VCFs to get common SNPs between all isolates. merge them all into one vcf file.
 os.system("bcftools isec -p {} -n={} {}".format(pileup,len(samp_list),intersect_isolates))
+print("INTESECTING ALL VCFs: bcftools isec -p {} -n={} {}".format(pileup,len(samp_list),intersect_isolates))
 os.chdir(pileup)
-
+os.system('rm vcf_list.txt')
 os.system('ls | grep ".vcf" > vcf_list.txt')
 
 vcf_list = []
@@ -68,4 +73,5 @@ for i in range(len(vcf_list)):
     os.system("bcftools index {}.gz".format(vcf_list[i]))
     merge_vcf = merge_vcf + " {}.gz".format(vcf_list[i])
 
+print("MERGING THE VCF: bcftools merge {} > output.merge.vcf".format(merge_vcf))
 os.system("bcftools merge {} > output.merge.vcf".format(merge_vcf))
