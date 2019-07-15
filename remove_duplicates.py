dd4dd33ffbb12650cd4cad132c3044fe459fadfe
Noah Legall <noahaus@uga.edu>
2019-07-15 14:51:18 -0400

    fixing

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -10,7 +10,7 @@ import os # use in order to call commands from terminal script is called in
 print("""
 Duplicate reads are inevitable in the PCR process of creating the sequences we study
 and if they stay within our BAM file, they could skew later downstream analysis results
-by possibly inflating the importance of certain ‘called SNPs’ especially if these variants are
+by possibly inflating the importance of certain 'called SNPs' especially if these variants are
 sequencing errors that were replicated. It is best to just remove them all together.
 """)
 #read in the R1.txt & R2.txt to variables R1 and R2 respectively
