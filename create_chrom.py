99d5614f5fb783a97c12dcdf88c6888db12a687b
Noah Legall <noahaus@uga.edu>
2019-07-18 16:05:51 -0400

    fixing

--- create_chrom.py
+++ create_chrom.py
@@ -16,7 +16,7 @@ for record in SeqIO.parse(ref_genome, "fasta"):
     step = 100000
     if step < total_len:
         for chunk in range(min_number, total_len, step)[1:]:
-            chrom_ranges.write("{}:{}-{}".format(chrom, min_number, chunk))
+            chrom_ranges.write("{}:{}-{}\n".format(chrom, min_number, chunk))
             min_number = chunk
-    chrom_ranges.write("{}:{}-{}".format(chrom, min_number, total_len))
+    chrom_ranges.write("{}:{}-{}\n".format(chrom, min_number, total_len))
 chrom_ranges.close()
