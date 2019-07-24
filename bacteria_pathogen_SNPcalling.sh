2813a08e9768374d8e5817b7c5c48b51631a0c49
Noah Legall <noahaus@uga.edu>
2019-07-24 16:10:22 -0400

    testing

--- bacteria_pathogen_SNPcalling.sh
+++ bacteria_pathogen_SNPcalling.sh
@@ -18,6 +18,8 @@ module add RAxML/8.2.11-foss-2016b-sse
 module add FASTX-Toolkit/0.0.14-foss-2016b
 module add Trimmomatic/0.36-Java-1.8.0_144
 
+echo $1
+
 REF= $1  #Variable for the reference genome. first argument
 EMAIL= $2  #Variable for the email you wish to get notifications from. second arguement
   #add modules the Sapelo2 way
